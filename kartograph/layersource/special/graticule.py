7d8acc6dddd51eec9db0dbdfe01c3b1f5700d719
Gregor Aisch <contact@vis4.net>
2012-03-06 16:06:12 +0100

    fixed graticule

--- kartograph/layersource/special/graticule.py
+++ kartograph/layersource/special/graticule.py
@@ -2,12 +2,12 @@
 from kartograph.geometry import Line, Feature
 from kartograph.layersource.layersource import LayerSource
 
+
 class GraticuleLayer(LayerSource):
     """
     special layer source for grid of longitudes and latitudes (graticule)
     """
-    
-    def get_features(self, latitudes, longitudes, proj, bbox=[-180,-90,180,90]):
+    def get_features(self, latitudes, longitudes, proj, bbox=[-180, -90, 180, 90]):
         """
         returns a list of line features that make up
         the graticule
@@ -16,43 +16,45 @@ class GraticuleLayer(LayerSource):
         maxLat = min(proj.maxLat, bbox[3])
         minLon = bbox[0]
         maxLon = bbox[2]
-        
+
         def xfrange(start, stop, step):
             while (step > 0 and start < stop) or (step < 0 and start > step):
                 yield start
                 start += step
-        
+
         line_features = []
         # latitudes
         for lat in latitudes:
             if lat < minLat or lat > maxLat:
                 continue
             pts = []
-            props = { 'lat': lat }
-            for lon in xfrange(-180,181,0.5):
+            props = {'lat': lat}
+            for lon in xfrange(-180, 181, 0.5):
                 if lon < minLon or lon > maxLon:
                     continue
-                pts.append((lon, lat))                    
+                if proj._visible(lon, lat):
+                    pts.append((lon, lat))
             if len(pts) > 1:
                 line = Feature(Line(pts), props)
                 line_features.append(line)
-        
-        # longitudes            
+
+        # longitudes
         for lon in longitudes:
             if lon < minLon or lon > maxLon:
                 continue
             pts = []
-            props = { 'lon': lon }
+            props = {'lon': lon}
             #lat_range = xfrange(step[0], 181-step[0],1)
             #if lon % 90 == 0:
             #    lat_range = xfrange(0, 181,1)
-            for lat in xfrange(0, 181,1):
-                lat_ = lat-90
+            for lat in xfrange(0, 181, 1):
+                lat_ = lat - 90
                 if lat_ < minLat or lat_ > maxLat:
                     continue
-                pts.append((lon, lat_))
+                if proj._visible(lon, lat_):
+                    pts.append((lon, lat_))
             if len(pts) > 1:
                 line = Feature(Line(pts), props)
                 line_features.append(line)
-    
+
         return line_features
--- kartograph/proj/base.py
+++ kartograph/proj/base.py
@@ -16,66 +16,65 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import math 
-from math import radians as rad
+import math
 
 
 class Proj(object):
     """
     base class for projections
-    """    
+    """
     HALFPI = math.pi * .5
     QUARTERPI = math.pi * .25
-    
+
     minLat = -90
     maxLat = 90
-                    
+
     def plot(self, polygon, truncate=True):
         points = []
         ignore = True
-        for (lon,lat) in polygon:
+        for (lon, lat) in polygon:
             vis = self._visible(lon, lat)
             if vis:
                 ignore = False
-            x,y = self.project(lon, lat)
+            x, y = self.project(lon, lat)
             if not vis and truncate:
-                points.append(self._truncate(x,y))
+                points.append(self._truncate(x, y))
             else:
-                points.append((x,y))
+                points.append((x, y))
         if ignore:
             return None
         return [points]
-        
+
     def ll(self, lon, lat):
-        return (lon,lat)
-    
+        return (lon, lat)
+
     def project(self, lon, lat):
         assert False, 'Proj is an abstract class'
-                
+
     def _visible(self, lon, lat):
         assert False, 'Proj is an abstract class'
-    
+
     def _truncate(self, x, y):
         assert False, 'truncation is not implemented'
-    
-    def world_bounds(self, bbox, llbbox=(-180,-90,180,90)):
-        sea = self.sea_shape(llbbox)
-        for x,y in sea:
-            bbox.update((x,y))
-        return bbox        
 
-    def sea_shape(self, llbbox=(-180,-90,180,90)):
+    def world_bounds(self, bbox, llbbox=(-180, -90, 180, 90)):
+        sea = self.sea_shape(llbbox)
+        for x, y in sea:
+            bbox.update((x, y))
+        return bbox
+
+    def sea_shape(self, llbbox=(-180, -90, 180, 90)):
         """
         returns non-projected multi-polygon map bounds
         """
         sea = []
         out = []
-        
+
         minLon = llbbox[0]
         maxLon = llbbox[2]
         minLat = max(self.minLat, llbbox[1])
         maxLat = min(self.maxLat, llbbox[3])
-        
+
         def xfrange(start, stop, step):
             if stop > start:
                 while start < stop:
@@ -85,36 +84,40 @@ class Proj(object):
                 while stop < start:
                     yield start
                     start -= step
-    
-        lat_step = abs((maxLat - minLat)/180.0)
-        lon_step = abs((maxLon - minLon)/360.0)
-        
-        for lat in xfrange(minLat, maxLat, lat_step): sea.append((minLon,lat))
-        for lon in xfrange(minLon, maxLon, lon_step): sea.append((lon, maxLat))
-        for lat in xfrange(maxLat, minLat, lat_step): sea.append((maxLon, lat))
-        for lon in xfrange(maxLon, minLon, lon_step): sea.append((lon, minLat))
-        
+
+        lat_step = abs((maxLat - minLat) / 180.0)
+        lon_step = abs((maxLon - minLon) / 360.0)
+
+        for lat in xfrange(minLat, maxLat, lat_step):
+            sea.append((minLon, lat))
+        for lon in xfrange(minLon, maxLon, lon_step):
+            sea.append((lon, maxLat))
+        for lat in xfrange(maxLat, minLat, lat_step):
+            sea.append((maxLon, lat))
+        for lon in xfrange(maxLon, minLon, lon_step):
+            sea.append((lon, minLat))
+
         for s in sea:
             lon, lat = s
             out.append(self.project(lon, lat))
-        
-        return [out]    
-        
+
+        return [out]
+
     def __str__(self):
-        return 'Proj('+self.name+')'
-        
+        return 'Proj(' + self.name + ')'
+
     def toXML(self):
         from svgfig import SVG
         p = SVG('proj', id=self.name)
         return p
-    
+
     @staticmethod
     def attributes():
         """
-        returns array of attribute names of this projection 
+        returns array of attribute names of this projection
         """
         return []
-    
+
     @staticmethod
     def fromXML(xml):
         id = xml['id']
@@ -126,4 +129,4 @@ class Proj(object):
                     args[prop[0]] = float(val)
             return ProjClass(**args)
         raise Exception("could not restore projection from xml")
-        
+
