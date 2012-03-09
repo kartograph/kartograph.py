ec74fee01bcb6e7466bcf0d0208efd98ffd99b0e
Gregor Aisch <contact@vis4.net>
2012-03-09 15:18:23 +0100

    cleaned white spaces

--- kartograph/simplify/mpoint.py
+++ kartograph/simplify/mpoint.py
@@ -12,24 +12,26 @@ class MPoint:
         self.two = False
         self.three = False
         self.keep = False
-        
+
     def isDeletable(self):
-        if self.keep or self.simplified or self.three: 
+        if self.keep or self.simplified or self.three:
             return False
         return True
-        
+
     def __repr__(self):
         return 'Pt(%.2f,%.2f)' % (self.x, self.y)
-        
+
     def __len__(self):
         return 2
-        
+
     def __getitem__(self, key):
-        if key == 0: return self.x
-        if key == 1: return self.y
+        if key == 0:
+            return self.x
+        if key == 1:
+            return self.y
         raise IndexError()
-        
+
     def __contains__(self, key):
-        if key == "deleted": return True
+        if key == "deleted":
+            return True
         return False
-        
