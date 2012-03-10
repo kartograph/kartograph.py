e207d78959fd53d59a80f6982e47b34e0e0934ef
Gregor Aisch <contact@vis4.net>
2012-03-10 13:18:44 +0100

    simplified command line interface, you can pass map configuration as second argument now

--- kartograph/cli.py
+++ kartograph/cli.py
@@ -13,7 +13,7 @@ def main():
     from errors import KartographError
 
     if len(sys.argv) < 2:
-        print "try: kartograph generate"
+        print "try: kartograph svg map-config.yaml"
         sys.exit(1)
 
     command = sys.argv[1]
@@ -22,23 +22,31 @@ def main():
 
         cfg = {}
         output = None
+        opt_src = None
         opts, args = getopt.getopt(sys.argv[2:], 'c:o:', ['config=', 'output='])
         for o, a in opts:
             if o in ('-c', '--config'):
                 opt_src = a
-                if os.path.exists(opt_src):
-                    t = open(opt_src, 'r').read()
-                    if opt_src[-5:].lower() == '.json':
-                        cfg = json.loads(t)
-                    elif opt_src[-5:].lower() == '.yaml':
-                        import yaml
-                        cfg = yaml.load(t)
-                    else:
-                        raise KartographError('supported config formats are .json and .yaml')
-                else:
-                    raise KartographError('config json not found')
             elif o in ('-o', '--output'):
                 output = a
+
+        if opt_src is None and len(sys.argv) > 2:
+            opt_src = sys.argv[2]
+        else:
+            raise KartographError('you need to pass a map configuration (json/yaml)')
+
+        # check and load map configuration
+        if os.path.exists(opt_src):
+            t = open(opt_src, 'r').read()
+            if opt_src[-5:].lower() == '.json':
+                cfg = json.loads(t)
+            elif opt_src[-5:].lower() == '.yaml':
+                import yaml
+                cfg = yaml.load(t)
+            else:
+                raise KartographError('supported config formats are .json and .yaml')
+        else:
+            raise KartographError('configuration not found')
 
         K = Kartograph()
 
