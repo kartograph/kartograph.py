a693794a38f26a7b02c25bad43091105eb0c0a49
Gregor Aisch <contact@vis4.net>
2012-04-03 11:01:09 +0200

    increased minor version since the cli changed

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ Open Source Python library for generating semantic SVG maps.
 
 setup(
     name='kartograph.py',
-    version='0.1.9',
+    version='0.2.0',
     description="Open Source Python library for generating semantic SVG maps",
     long_description=long_desc,
     classifiers=[
